d5a66d0390a04b75020f7d52b8b9ceec3f32e427
Kristopher Ruzic <krruzic@gmail.com>
2017-02-16 20:16:47 -0700

    works and sends files correctly

--- FTPClient.java
+++ FTPClient.java
@@ -3,55 +3,117 @@
  * FastFtp implements a basic FTP application based on UDP data transmission and 
  * alternating-bit stop-and-wait concept
  * @author      Kristopher Ruzic
- * @version     1.0, 1 Feb 2017
+ * @version     1.5, 16 Feb 2017
  *
- */
+ 
+*/
 
-import java.util.Timer;
 import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketTimeoutException;
+import java.net.InetAddress;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+
 public class FTPClient {
 
     private String file_name;
-    private Timer t; 
     private Socket tcpSock; 
+    private DatagramSocket udpSock;
+    private DataInputStream tcpIn;
+    private DataOutputStream tcpOut;
+    private byte[] outgoing;    
+    private FileInputStream fis;
+    private int server_port;
     /**
      * Constructor to initialize the program 
      * 
      * @param serverName    server name
      * @param server_port   server port
-     * @param file_name     name of file to transfer
-     * @param timeout       Time out value (in milli-seconds).
+     * @param file_name     name of file to transfer * @param timeout       Time out value (in milli-seconds).
      */
-    
-    public FTPClient(String server_name, int server_port, String file_name, int timeout) {
-    
-    /* Initialize values */
-        this.tcpSock = new Socket(server_name, server_port);        
-        this.file_name = file_name; 
-        this.t = new Timer(timeout);
-        doHandShake(); 
-    }
-    
 
-    /**
-     * Send file content as Segments
-     * 
+    // sets up all the attributes and calls the handshake routine
+    public FTPClient(String server_name, int server_port, String file_name, int timeout) { 
+        try {
+            this.tcpSock = new Socket(server_name, server_port); 
+            this.tcpSock.setReuseAddress(true);
+            this.file_name = file_name; 
+            doHandshake();
+            this.udpSock = new DatagramSocket();
+            this.udpSock.setSoTimeout(timeout*10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.server_port = server_port;
+    }
+
+    /*
+     * method that does the sending. Reads file chunk by chunk,
+     * creates Segments then sends them in a datagram packet
+     * calls waitForACK to ensure it was sent
+     * also does the closing handshake
      */
-    public void send() {
+    public void send() throws Exception { 
+        InetAddress hostAddress = InetAddress.getByName("localhost");
+        fis = new FileInputStream(new File(file_name)); 
+        Segment s = new Segment();
+        this.outgoing = new byte[512];
+        int count = 0;
+        int a;
+        int seq = 1;
+        while((a = fis.read(outgoing,0,outgoing.length)) != -1) {
+            seq = (seq == 1) ? 0 : 1;
+            s.setPayload(outgoing);
+            s.setSeqNum(seq);
+            DatagramPacket dp = new DatagramPacket(s.getBytes(), s.getLength(), hostAddress, this.server_port);
+            this.udpSock.send(dp); 
+            waitForACK(dp,count++,seq);
+        }
+        fis.close();
+        this.tcpOut.writeByte(0);
+       
+        // clean up
+        this.tcpSock.close();
+        this.udpSock.close();
+    }
+
+    // loops infinitely until it recieves an ACK for the packet
+    public void waitForACK(DatagramPacket dp, int count, int expectedSeq) throws Exception {
+        byte[] incoming = new byte[1];
+        while (true) {
+            DatagramPacket resp = new DatagramPacket(incoming, incoming.length);
+            try {
+                udpSock.receive(resp);
+                if (incoming[0] != expectedSeq)
+                    System.out.println("duplicate ACK, ignoring");
+            } catch (SocketTimeoutException e) {
+                System.out.format("resending packet %d!\n", count);
+                this.udpSock.send(dp);
+                continue;
+            }
+            System.out.format("packet %d sent and ACKed, moving to next segment\n", count);
+            break;
+        }
+    }
+
+    // writes one byte on the tcp socket and checks if it got a response
+    public void doHandshake() throws FTPServerException, java.io.IOException {
+        this.tcpOut = new DataOutputStream(this.tcpSock.getOutputStream());
+        this.tcpOut.writeUTF(this.file_name);
+        this.tcpOut.flush();
+        this.tcpIn = new DataInputStream(this.tcpSock.getInputStream());
+        byte resp = this.tcpIn.readByte();
         
-        /* send logic goes here. You may introduce addtional methods and classes
+        if (resp == 0) System.out.println("Handshake done!");
+        else throw new FTPServerException("Could not complete handshake with server");
     }
 
-    
-
-/**
-        * A simple test driver
-         * 
-        */
     public static void main(String[] args) {
         
         String server = "localhost";
@@ -77,7 +139,13 @@ public class FTPClient {
         FTPClient ftp = new FTPClient(server, server_port, file_name, timeout);
         
         System.out.printf("sending file \'%s\' to server...\n", file_name);
-        ftp.send();
+        try {
+            ftp.send();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        
         System.out.println("file transfer completed.");
     }
 
---
+++ FTPServerException.java
@@ -0,0 +1,6 @@
+public class FTPServerException extends Exception {
+  public FTPServerException() { super(); }
+  public FTPServerException(String message) { super(message); }
+  public FTPServerException(String message, Throwable cause) { super(message, cause); }
+  public FTPServerException(Throwable cause) { super(cause); }
+}
